5cae665d67b28d2fba7e45d78f53437c8e432324
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-07 04:10:10 +0000

    [Add]詳細画面の追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,9 +3,27 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new
   end
 
+  def create
+    post_image = PostImage.new(post_image_params)
+    # 投稿のIDにログイン中のユーザのIDを代入し、現在のユーザが投稿したようにする
+    # user_idは投稿時に指定していない（ユーザには自分のuserモデルのIDは分からない）からここで指定してあげる必要がある
+    post_image.user_id = current_user.id
+    post_image.save
+    redirect_to post_images_path
+  end
+
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
+
+
+private
+def post_image_params
+  params.require(:post_image).permit(:image, :shop_name, :caption)
+end
+
 end
