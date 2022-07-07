ed962c15501d5c77efc9e68904585dfbd59a2c95
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-07 09:35:28 +0000

    [Add]userモデルの編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'homes/about', as: 'about'
   root to: 'homes#top'
   resources:post_images, only: [:new, :create, :show, :index, :destroy]
-  resources:users, only: [:show, :edit]
+  resources:users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
